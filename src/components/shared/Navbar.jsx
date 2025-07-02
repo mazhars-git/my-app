14bd5cb444adeebe50e2f6d818a470c9ba6d31e3
mazhars-git <mazharulislam611@gmail.com>
2025-07-03 00:00:05 +0600

    up

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
-import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-amber-500 text-white py-4 shadow-md font-bold">
+    <nav className="bg-orange-500 text-white py-4 shadow-md font-bold">
       <div className="container mx-auto flex justify-between items-center">
-        <a href="/" className="text-2xl">
+        <Link href="/" className="text-2xl">
           MyApp
-        </a>
+        </Link>
         <div className="space-x-6">
           <Link href="/Home">Home</Link>
           <Link href="/Home">Contact</Link>
